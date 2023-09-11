149aa80d49dde5baf73c9a35cb1b9eb6b16a0501
Ramy Ouda <ramyouda@gmail.com>
2023-09-11 18:59:20 -0400

    delete route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AgGridModule } from 'ag-grid-angular';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, AgGridModule, HttpClientModule],
+  imports: [BrowserModule, AgGridModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
